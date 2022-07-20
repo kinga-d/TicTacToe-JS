e2e0ad308dd1d7a1855e49140be1c26f6bfeba61
Kinga Danielewicz <danielewicz.kinga@gmail.com>
2022-07-21 00:17:52 +0200

    basic js

--- TicTacToe-JS.js
+++ TicTacToe-JS.js
@@ -1,1 +1,11 @@
 // JavaScript source code
+
+const fields = document.querySelectorAll(".cell");
+
+let activePlayer = 'X';
+
+fields.forEach((field) => {
+    field.addEventListener('click', (e) => {
+        e.target.classList.add('cell--$[activePlayer]');
+    })
+})
